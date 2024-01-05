fc4fdf33c7276c61ba04ff293c46e95de4fd05ee
uzairgabol <gaboluzair0@gmail.com>
2024-01-06 03:49:32 +0500

    added error handling

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDatabase } from "./config/dbConnect.js";
 import productRoutes from "./routes/products.js";
+import errorMiddleware from "./middlewares/errors.js";
+
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down server due to Uncaught Exception");
+  process.exit(1);
+});
 
 const app = express();
 
@@ -12,6 +19,16 @@ app.use(express.json());
 
 app.use("/api/v1", productRoutes);
 
-app.listen(process.env.PORT, () => {
+app.use(errorMiddleware);
+
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server started running on PORT ${process.env.PORT}`);
+});
+
+//Unhandeled Promise Rejection
+
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down server due to Unhandled Promise Rejection");
+  process.exit(1);
 });
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,48 +1,58 @@
 import Product from "../models/product.js";
+import errorMiddleware from "../middlewares/errors.js";
+import ErrorHandler from "../utils/errorHandler.js";
+import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+import APIFilters from "../utils/apiFilters.js";
 
 //All Product => /api/v1/products
-export const getProducts = async (req, res) => {
-  const products = await Product.find();
+export const getProducts = catchAsyncErrors(async (req, res) => {
+  const resPerPage = 4;
+  const apiFilters = new APIFilters(Product, req.query).search().filters();
+
+  let products = await apiFilters.query;
+  let filteredProductsCount = products.length;
+
+  apiFilters.pagination(resPerPage);
+  products = await apiFilters.query.clone();
+
   res.status(200).json({
+    resPerPage,
+    filteredProductsCount,
     products,
   });
-};
+});
 
 //Create new Product => /api/v1/admin/products
 
-export const newProduct = async (req, res) => {
+export const newProduct = catchAsyncErrors(async (req, res) => {
   const product = await Product.create(req.body);
 
   res.status(200).json({
     product,
   });
-};
+});
 
 //Get Single Product => /api/v1/products/:id
 
-export const getProductDetails = async (req, res) => {
+export const getProductDetails = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req?.params?.id);
 
   if (!product) {
-    return res.status(404).json({
-      error: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
 
   res.status(200).json({
     product,
   });
-};
+});
 
 //Update Product => /api/v1/products/:id
 
-export const updateProduct = async (req, res) => {
+export const updateProduct = catchAsyncErrors(async (req, res) => {
   let product = await Product.findById(req?.params?.id);
 
   if (!product) {
-    return res.status(404).json({
-      error: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
 
   product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {
@@ -52,17 +62,15 @@ export const updateProduct = async (req, res) => {
   res.status(200).json({
     product,
   });
-};
+});
 
 //Delete Product => /api/v1/products/:id
 
-export const deleteProduct = async (req, res) => {
+export const deleteProduct = catchAsyncErrors(async (req, res) => {
   let product = await Product.findById(req?.params?.id);
 
   if (!product) {
-    return res.status(404).json({
-      error: "Product not found",
-    });
+    return next(new ErrorHandler("Product not found", 404));
   }
 
   await product.deleteOne();
@@ -70,4 +78,4 @@ export const deleteProduct = async (req, res) => {
   res.status(200).json({
     message: "Product Deleted",
   });
-};
+});

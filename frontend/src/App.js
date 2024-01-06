ed21724b9771b75dd7378a3bc2646c7da5eac8aa
uzairgabol <gaboluzair0@gmail.com>
2024-01-07 04:31:00 +0500

    Added Some FrontEnd Components

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import "./App.css";
+import Home from "./components/Home";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div class="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
